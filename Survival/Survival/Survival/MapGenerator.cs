5df5f920f29f0e2fa2717923bfeb8a4721c3b1eb
Fredrik <f.wigarde@hotmail.com>
2014-02-26 16:15:52 +0100

    Början på tile system

---
+++ Survival/Survival/Survival/MapGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Survival
+{
+    class MapGenerator
+    {
+
+        public enum MapSize { 
+            Small,
+            Medium,
+            Large,
+            Huge
+        }
+
+        MapSize m_mapSize = MapSize.Small;
+
+        public static List<Tile> tileList = new List<Tile>();
+        int m_mapTiles;
+
+        public MapGenerator() {
+            Init();
+        }
+
+        private void Init() {
+            GenerateMap();
+        }
+
+        private void GenerateMap() {
+            Random rand = new Random();
+            Tile currTile;
+            switch (m_mapSize) { 
+                case MapSize.Small:
+                    m_mapTiles = 40;
+                    break;
+                case MapSize.Medium:
+                    m_mapTiles = 80;
+
+                    break;
+                case MapSize.Large:
+                    m_mapTiles = 160;
+
+                    break;
+                case MapSize.Huge:
+                    m_mapTiles = 320;
+
+                    break;
+            }
+        }
+
+    }
+}
---
+++ Survival/Survival/Survival/Tile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Survival
+{
+    class Tile
+    {
+        public enum TileType { 
+            Grass,
+            Stone,
+            Water
+        }
+
+        public Tile() { 
+        
+        }
+    }
+}
